e03eb8b9d25b06dceed1f29f7b62906999242acb
Csige Róbert <xike@INF.ELTE.HU>
2016-02-23 17:19:13 +0100

    start project

---
+++ app/lib/calculator/index.spec.js
@@ -0,0 +1,15 @@
+'use strict';
+let Calculator = require('./');
+let chai = require('chai');
+let expect = 
+
+describe('calculator lib',
+function(){
+	describe('#add, function (){
+		it('should add the given two numbers', function() {
+			let calculator = new Calculator();
+			let result = calculator.add(1,2);
+			expect(result).to.eql(3);
+		});
+	});
+);
